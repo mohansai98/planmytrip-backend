ce50f8f0354a33e77a3115807baaf2bbe2f705d8
Mohan Sai Singu <mohansaisingu23@gmail.com>
2024-08-22 13:37:55 -0400

    heroku deployment

--- src/main/java/com/planmytrip/backend/config/SecurityConfig.java
+++ src/main/java/com/planmytrip/backend/config/SecurityConfig.java
@@ -83,7 +83,6 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(allowedOrigins);
-        System.out.println(allowedOrigins);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
