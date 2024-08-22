ce8f8c0398e9e8e2c62f73c7f2488da2e7bc1b36
Mohan Sai Singu <mohansaisingu23@gmail.com>
2024-08-22 14:51:07 -0400

    fixing errors

--- src/main/java/com/planmytrip/backend/config/SecurityConfig.java
+++ src/main/java/com/planmytrip/backend/config/SecurityConfig.java
@@ -83,8 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(allowedOrigins);
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
